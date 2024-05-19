d20b7148ed9a9a95f3fe54796d96346195d017d1
nguyendinhtuanx31@gmail.com <nguyendinhtuanx31@gmail.com>
2024-05-19 12:11:47 +0700

    cập nhật

--- DataServices.cs
+++ DataServices.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace DO_AN_KI_2
@@ -21,16 +22,50 @@ namespace DO_AN_KI_2
         {
             connection.Close();
         }
-
-        public void ExcutteNonqueries()
-        {
-            SqlCommand command = new SqlCommand();
-            command.ExecuteNonQuery();
-        }
         public void ShowErrorMessageBox(string errorMessage, string errorTitle = "Thông báo")
         {
             MessageBox.Show(errorMessage, errorTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        /// <summary>
+        /// dùng khi delete , cập nhật , hoặc insert
+        /// </summary>
+        /// <param name="Query_"></param>
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, connection);
+            cmd.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// dùng khi cần lấy dữ liệu riêng lẻ 
+        /// </summary>
+        /// <param name="Query_"></param>
+        /// <returns></returns>
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, connection);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+
+        /// <summary>
+        /// Dùng khi show liên quan đến datasource
+        /// </summary>
+        /// <param name="Query_"></param>
+        /// <returns></returns>
+        public object ShowObjectData(string Query_)
+        {
+            OpenDB();
+            SqlDataAdapter dr = new SqlDataAdapter(Query_, Sql);
+            DataSet ds = new DataSet();
+            dr.Fill(ds);
+            object dataum = ds.Tables[0];
+            CloseDB();
+            return dataum;
+        }
+
 
         // Method for logging errors in a file or a logging system.
 
