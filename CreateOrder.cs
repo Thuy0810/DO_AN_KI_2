ff5ddb6f5238b2f942b01d594befb012c0ed99f3
nguyendinhtuanx31@gmail.com <nguyendinhtuanx31@gmail.com>
2024-06-01 10:30:02 +0700

    cập nhật

--- CreateOrder.cs
+++ CreateOrder.cs
@@ -47,9 +47,11 @@ namespace DO_AN_KI_2
                 employ.Text = employName;
                 dateOrder.Text = DateTime.Now.ToString();
                 this.ControlBox = false;
+                label1.Text = "XEM ĐƠN HÀNG";
             }
             else
             {
+                AddCustomer.Visible = false;
                 employ.Text = model.employName;
                 dateOrder.Text = model.date;
                 customerSelect.SelectedValue = model.customer;
